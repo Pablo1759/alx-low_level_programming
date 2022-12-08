d2d10d3b05f1d518cb035c4e761d2b122c5d697d
Pablo1759 <adebolarotimi1@gmail.com>
2022-12-08 09:52:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Prints exactly and that piece of art is ueseful
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
-	printf("and that piece of art is useful\" - Dora Kprpar, 2015-10=19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 205-10-19\n", 59);
 	return (1);
 
 }
