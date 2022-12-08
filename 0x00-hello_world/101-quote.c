b2b77ad711b14d02d0f5fdbb6c5be863e9ffe15c
Pablo1759 <adebolarotimi1@gmail.com>
2022-12-08 09:56:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints exactly and that piece of art is ueseful
+ * main - prints exactly and that piece of art is ueseful
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 205-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
