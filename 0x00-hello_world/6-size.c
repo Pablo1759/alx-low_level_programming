f0a1ad5357b3e51f7ab7a9e6c9908223e1b04b34
Pablo1759 <adebolarotimi1@gmail.com>
2022-12-08 07:15:00 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,19 @@
 
 
 /**
- * main - Entry point
+ * main - Prints the sizeof many var types
  *
  * Return: Always 0 (Success)
  */
 
 int main (void)
 {
+
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Sie of an int: %lu byte(s)\n", sizeof(int));
