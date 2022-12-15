0d91d084748c5fbfd82b2925989cc933ae8a6263
Pablo1759 <adebolarotimi1@gmail.com>
2022-12-15 04:30:45 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,8 +14,8 @@ void print_square(int size)
 		for (; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-					_putchar(35);
-					_putchar('\n');
+				_putchar(35);
+			_putchar('\n');
 		}
 	}
 	else
